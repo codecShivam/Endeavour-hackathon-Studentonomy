a4767e9c12ff7003047f8583f182438d58a62f5a
Sam <asamadans@gmail.com>
2023-05-18 19:46:35 +0530

    added django

---
+++ studentonomy/src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </div>
+    );
+};
+
+export default Homepage;
